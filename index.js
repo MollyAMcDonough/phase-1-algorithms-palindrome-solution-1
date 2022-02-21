fab022d626f80b70a382035e888849119acfd98d
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-02-21 14:30:18 -0500

    commit tree 2bd80fa954c436df0d3bf55659c533d53d84e7e2

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+function reverse(word) {
+  const wordArray = word.split("");
+  const reversedArray = wordArray.reverse();
+  return reversedArray.join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word)
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  write a function that returns true if the given string is a palindrome
+  and false if it isn't. 
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
 /*
@@ -15,11 +29,16 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("mom", isPalindrome("mom"))
+  console.log("a",isPalindrome("a"))
+  console.log("empty", isPalindrome(""))
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("palindrome", isPalindrome("palindrome"))
+  console.log("Mom", isPalindrome("Mom"))
 }
 
 module.exports = isPalindrome;
